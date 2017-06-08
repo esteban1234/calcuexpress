ce0780872e246c21d6546d07183968f4e7c9a614
esteban1234 <tva_libra@hotmail.com>
2017-06-08 18:00:04 -0500

    inicio de pagina

---
+++ index.php
@@ -0,0 +1,43 @@
+
+<?php include 'vistas/encabezado.php' ?>
+<body>
+
+
+<div class="banner">
+	<div class="contenlogomenu">
+		<figure class="logo">
+			<img src="img/calcu.png" alt="" class="img-responsive pull-left">
+		</figure>
+
+		<nav class=" menu pull-right">
+			<ul>
+				<li><a class="select" href="">INDEX</a></li>
+				<li><a href="">NOSOTROS</a></li>
+				<li><a href="">SERVICIOS</a></li>
+				<li><a href="">CONTACTO</a></li>
+			</ul>
+		</nav>
+	</div>
+	<div class="contenpuntosimg">
+		<div class="puntos pull-left">
+			<p class="puntoss"><span><i class="fa fa-check" aria-hidden="true"></i></span>&nbsp;Supervisión fiscal integral</p>
+            <p class="puntoss"><span><i class="fa fa-check" aria-hidden="true"></i></span>&nbsp;Planeación patrimonial</p>
+            <p class="puntoss"><span><i class="fa fa-check" aria-hidden="true"></i></span>&nbsp;Estructuras extraterritoriales</p>
+            <p class="puntoss"><span><i class="fa fa-check" aria-hidden="true"></i></span>&nbsp;Litigio fiscal y administrativo</p>
+            <p class="puntoss"><span><i class="fa fa-check" aria-hidden="true"></i></span>&nbsp;Auditoria de control interno</p>
+            <p class="puntoss"><span><i class="fa fa-check" aria-hidden="true"></i></span>&nbsp;Derecho notarial</p>
+            <p class="puntoss"><span><i class="fa fa-check" aria-hidden="true"></i></span>&nbsp;Administración de nómina</p>
+		</div>
+		<div class="img pull-right"></div>
+	</div>
+</div>
+
+
+ 
+<?php include 'vistas/pie.php' ?>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/slide.js"></script>
+<script src="js/menu.js"></script>
+</body>
+</html>
---
+++ vistas/pie.php
@@ -0,0 +1,3 @@
+<footer>
+<p>Derechos de autor 2017 © | CALCUEXPRESS REPARADORES DE EMPRESAS S.C.P</p>
+</footer>
