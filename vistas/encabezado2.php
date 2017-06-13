ecf8bfd0e38129dc54ca68f4ebf84f524faba6ae
esteban1234 <tva_libra@hotmail.com>
2017-06-13 13:31:56 -0500

    metas

--- vistas/encabezado2.php
+++ vistas/encabezado2.php
@@ -2,13 +2,13 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	
+	<meta name="google-site-verification" content="F2RTVS4QazKV6b3u5fKhSroWgIlz470F0OC9ngaVKOU" />
 	<link rel="icon" type="image/png" href="../img/favicon.png" />
 	<meta name="theme-color" content="#000">
 	<title>CALCUEXPRESS REPARADORES DE EMPRESAS</title>
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
   	<META NAME="Title" content="CALCUEXPRESS REPARADORES DE EMPRESAS">
-  	<META NAME="Author" CONTENT="[]"/>
+  	<META NAME="Author" CONTENT="[http://calcuexpress.com/]"/>
   	<META name="Description" content="Somos una compañía dedicados a ofrecer servicios de consultoria financiera, estrategia fiscal, Auditoria legal, Administración de nomina. Etc."/>
      <META NAME="Keywords" CONTENT="Administradora de nominas, Auditoria fiscal, consultoria financiera."/>
   	<link rel="stylesheet" href="../css/bootstrap.css">
